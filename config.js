539fbdbc16104434311660d401b56f05d5ba9bc9
bettyna0610 <50782390+bettyna0610@users.noreply.github.com>
2021-11-16 17:47:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB: 'mongodb+srv://Bettina:social-media-app@cluster0.r61iq.mongodb.net/merng?retryWrites=true&w=majority',
-    SECRET_KEY: 'some very secret key'
+    MONGODB: '',
+    SECRET_KEY: ''
 }
